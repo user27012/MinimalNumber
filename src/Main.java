67771cb24c541b7b12248bd0d3dbcef07edc6a76
user27012 <kolabelozorov@gmail.com>
2023-10-27 20:28:04 +0200

    HW - 08 - 04

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,49 @@
 import java.util.LinkedList;
-import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<String> spisok = new LinkedList<>();
-        spisok.add("WAKEUP");
-        spisok.add("SCHOOL");
-        spisok.add("RUST");
-        spisok.add("WORK");
-        spisok.add("REPEAT");
-
-        LinkedList<String> doublelist = new LinkedList<>();
-
-        for (String letter : spisok) {
-            //Переменная doubleword будет принимать значение каджой буквы
-            String doubleword = "";
-            for (int i = 0; i < letter.length(); i++){
-                char word = letter.charAt(i);
-                //Отвечает за подвоение каждой буквы
-                doubleword += word;
-                doubleword += word;
-            }
-            //Отвечает за добовление каждого слова в список
-            doublelist.add(doubleword);
+        //Создаем переменную что определит нужное количество чисел для массива
+        int N = 10;
+        //Запрашиваем числа, отправляем данные про наш масив
+        LinkedList<Integer> integerList = getIntegerList(N);
+        //Выводим полученные числа
+        System.out.println("Случайные 10 чисел: ");
+        for (Integer number : integerList) {
+            System.out.print(number + ", " );
         }
-        //Тут Список выводится -
-        for (String doubleword : doublelist) {
-            System.out.println(doubleword);
-        }
+        System.out.println("");
+        //Тут получаем минимальное число
+        System.out.println("Минимальное число: ");
+        System.out.println(getMinimum(integerList));
+
     }
+
+    public static LinkedList<Integer> getIntegerList(int N) {
+        //Переносим наш массив, создаем рандом
+        LinkedList<Integer> integerList = new LinkedList<>();
+        Random random = new Random();
+
+        //Цикл что отвечает за назначение объектов их случайными числами
+        for (int i = 0; i < N; i++) {
+            int randomNumber = random.nextInt(100);
+            integerList.add(randomNumber);
+        }
+        //Возврощаем полученный результат
+        return integerList;
+    }
+    public static int getMinimum(LinkedList<Integer> integerList){
+        //Условия в случае ошибки
+        if (integerList.isEmpty()){
+            throw new IllegalArgumentException("Список пуст");
+        }
+        //Получения минимального числа
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(integerList); // Створюємо пріоритетну чергу
+
+        //Возврат минимального числа
+        return priorityQueue.peek();
+    }
+
 }
+
